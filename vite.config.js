809abaca1797e805a1bae535618420048274aea6
mayarakok <akokmayar607@gmail.com>
2024-11-05 22:10:11 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/BONMAJ/',
+   base: '/',
   root:"./",
   plugins: [react()],
   build: {
-    outDir: 'dist', // The output directory for the build
+    outDir: 'dist/', // The output directory for the build
   },
 })
